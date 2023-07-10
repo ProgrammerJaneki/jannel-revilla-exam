494290d50fb40aed97a9ac065b2849bbec3e67e8
ProgrammerJaneki <revillazjannelz@gmail.com>
2023-07-10 14:52:46 +0800

    implemented prepared statements

--- sync_youtube_channel.php
+++ sync_youtube_channel.php
@@ -1,6 +1,6 @@
 <?php
-
-$apiKey = 'AIzaSyC5AGkcukgjgJXFaHBDdex9dLMDHrtAiGY';
+// $apiKey = 'AIzaSyC5AGkcukgjgJXFaHBDdex9dLMDHrtAiGY';
+$apiKey = 'AIzaSyD-bfRUd_roKr6T6cPeOyGy5uyryOSabLs';
 $channelId = 'UCWJ2lWNubArHWmf3FIHbfcQ';
 $maxResults = 100;
 
@@ -26,17 +26,48 @@ $countData = mysqli_fetch_assoc($countResult);
 $totalItems = $countData['total'];
 
 // query url
+$channelUrl = "https://youtube.googleapis.com/youtube/v3/channels?part=snippet&id={$channelId}&maxResults=1&key={$apiKey}";
 $url = "{$baseUrl}?part=snippet&channelId={$channelId}&maxResults={$maxResults}&order=date&key={$apiKey}";
 
-echo "INITIAL <br>";
+// Check if Youtube Name already exists
+$channelName = "NBA";
+$channelQuery = "SELECT * FROM youtube_channels WHERE name = ?";
+$stmtChannel = mysqli_prepare($conn, $channelQuery);
+mysqli_stmt_bind_param($stmtChannel, "s", $channelName);
+if (mysqli_stmt_execute($stmtChannel)) {
+    $nameResult = mysqli_stmt_get_result($stmtChannel);
+
+    if (mysqli_num_rows($nameResult) > 1) {
+        echo "Username already exists";
+    } else {
+        $channelRes = file_get_contents($channelUrl);
+        if ($channelRes) {
+            $channelData = json_decode($channelRes, true);
+
+            $firstItem = $channelData['items'][0];
+            $snippet = $firstItem['snippet'];
+            $name = $snippet['title'];
+            $description = $snippet['description'];
+            $profilePicture = $snippet['thumbnails']['default']['url'];
+
+            $insertChannelQuery = "INSERT INTO youtube_channels (profile_picture, name, description) VALUES (?, ?, ?)";
+
+            $stmtChannelInsert = mysqli_prepare($conn, $insertChannelQuery);
+            mysqli_stmt_bind_param($stmtChannelInsert, "sss", $profilePicture, $name, $description);
+
+            if (mysqli_stmt_execute($stmtChannelInsert)) {
+                echo "Channel added successfully!";
+            }
+        }
+    }
+}
+
 while (!$reachedMaxResults || $totalItems <= $maxResults) {
-    echo "CHECK";
 
     if (!empty($nextPageToken)) {
         $url .= "&pageToken={$nextPageToken}";
     }
 
-    // executes the given url
     $res = file_get_contents($url);
 
     if ($res) {
@@ -60,6 +91,7 @@ while (!$reachedMaxResults || $totalItems <= $maxResults) {
 
             $insertQuery = "INSERT INTO youtube_channel_videos (video_link, title, description, thumbnail) VALUES (?, ?, ?, ?)";
 
+            // prepares statement and bind values to the placeholders
             $stmt = mysqli_prepare($conn, $insertQuery);
             mysqli_stmt_bind_param($stmt, "ssss", $videoLink, $title, $description, $thumbnail);
 
